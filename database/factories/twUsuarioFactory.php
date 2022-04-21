fc33b1eed3f5a89cc84c08c2a85e37f5b355784e
Sennit <sennitoficial@gmail.com>
2022-04-21 12:11:59 -0500

    se crean 10 usuarios en la tabla twUsuarios

--- database/factories/twUsuarioFactory.php
+++ database/factories/twUsuarioFactory.php
@@ -19,12 +19,23 @@ class twUsuarioFactory extends Factory
             'S_Nombre' => $this->faker->name(),
             'S_Apellidos' => $this->faker->lastName(),
             'S_FotoPerfilUrl' => $this->faker->url(),
-            'S_Activo' => $this->faker->boolean(),
-            'password' => $this->faker->password(),
-            'verification_token' => $this->faker->randomDigit(),
-            'verified' => $this->faker->randomLetter(),
-            'created_at' => $this->faker->date(),
+            //'S_Activo' => $this->faker->boolean(),
+            'password' => '12345',
+            'verification_token' => $this->faker->md5(),
+            'verified' => 'DISABLED',
+            'created_at' => $this->faker->dateTimeThisMonth(),
             'updated_at' => $this->faker->dateTimeThisMonth(),
+
+           /*  'username' => 'CarlosDev',
+            'email' => 'carlos@gmail.com',
+            'S_Nombre' => 'Carlos',
+            'S_Apellidos' => 'Guzman',
+            'S_FotoPerfilUrl' => $this->faker->url(),
+            //'S_Activo' => $this->faker->boolean(),
+            'password' => '12345678',
+            'verification_token' => $this->faker->md5(),
+            'verified' => 'DISABLED',
+            'created_at' => $this->faker->dateTimeThisMonth(), */
         ];
     }
 }
