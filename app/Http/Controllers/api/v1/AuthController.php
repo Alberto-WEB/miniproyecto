6090ad744f52ff17d8fd181409de36e57f9361d8
Carlos Guzman <alberto.guzman.220793@gmail.com>
2022-04-24 09:41:17 -0500

    se agrega exception con try catch para recibir error en el login

--- app/Http/Controllers/api/v1/AuthController.php
+++ app/Http/Controllers/api/v1/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api\v1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Models\User;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -16,23 +17,22 @@ class AuthController extends Controller
     
     public function login(LoginRequest $request)
     {
-        $validated = $request->validated();
+        try {
+            if (!auth()->attempt($request->validated())) {
+                return response()->json([
+                    'msg' => 'BAD',
+                    'success' => false,
+                    'data' => [
+                        'msgError' => 'email o password no coinciden en la DB'
+                    ],
+                    'exeptions' => [
+                        'msgError' => null
+                    ],
+                    'time_execution' => microtime()
+            ], 422);
+            }
 
-        if (!auth()->attempt($validated)) {
-            return response()->json([
-                'msg' => 'BAD',
-                'success' => false,
-                'data' => [
-                    'msgError' => $validated
-                ],
-                'exeptions' => [
-                    'msgError' => 'email o password no coinciden en la DB'
-                ],
-                'time_execution' => microtime()
-        ], 422);
-        }
-
-        $token = auth()->user()->createToken('API Token')->accessToken;
+            $token = auth()->user()->createToken('API Token')->accessToken;
 
             return response()->json([
                 'msg' => 'OK',
@@ -40,10 +40,25 @@ class AuthController extends Controller
                 'data' => Auth::user(),
                 'access_token' => $token,
                 'exeptions' => [
-                    'msgError' => null,
+                    'msgError' => null
                 ],
                 'time_execution' => microtime()
             ], 200);
+
+        } catch (\Exception $exception) {
+            return response()->json([
+                'msg' => 'BAD',
+                'success' => false,
+                'data' => [
+                    'msgError' => 'Server error'
+                ],
+                'exeptions' => [
+                    'msgError' => $exception->getMessage()
+                ],
+                'time_execution' => microtime()
+        ], 500);
+           
+        } 
 
     }
 }
