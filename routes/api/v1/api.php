3963913a65eedccf625a42739576d1730aa7791a
Sennit <sennitoficial@gmail.com>
2022-04-25 11:28:03 -0500

    se crean rutas para CRUD de tw_usuarios

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\v1\AuthController;
 use App\Http\Controllers\api\v1\NewPasswordController;
-use App\Http\Controllers\api\v1\TwUsusariosController;
+use App\Http\Controllers\api\v1\TwUsuariosController;
 
 //Authentication
 Route::prefix('auth')->group(function () {
@@ -17,5 +17,9 @@ Route::post('reset-password', [NewPasswordController::class, 'resetPassword']);
 
 //CRUD TwUsuarios
 Route::prefix('usuarios')->group(function () {
-    Route::get('/list', [TwUsusariosController::class, 'index']);
+    Route::get('/list', [TwUsuariosController::class, 'index'])->middleware('auth:api');
+    Route::post('/create', [TwUsuariosController::class, 'store'])->middleware('auth:api');
+    Route::get('/show/{id}', [TwUsuariosController::class, 'show'])->middleware('auth:api');
+    Route::put('/update/{id}', [TwUsuariosController::class, 'update'])->middleware('auth:api');
+    Route::delete('/delete/{id}', [TwUsuariosController::class, 'destroy'])->middleware('auth:api');
 });
