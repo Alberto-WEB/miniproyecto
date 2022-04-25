a876a3ee22602a591bda4c0203e49ebe4d576fef
Sennit <sennitoficial@gmail.com>
2022-04-25 12:45:21 -0500

    se crean rutas para el CRUD de tw_corporativos

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\v1\AuthController;
 use App\Http\Controllers\api\v1\NewPasswordController;
+use App\Http\Controllers\api\v1\TwCorporativosController;
 use App\Http\Controllers\api\v1\TwUsuariosController;
 
 //Authentication
@@ -15,11 +16,20 @@ Route::prefix('auth')->group(function () {
 Route::post('forgot-password', [NewPasswordController::class, 'forgotPassword']);
 Route::post('reset-password', [NewPasswordController::class, 'resetPassword']);
 
-//CRUD TwUsuarios
+//CRUD tw_usuarios
 Route::prefix('usuarios')->group(function () {
     Route::get('/list', [TwUsuariosController::class, 'index'])->middleware('auth:api');
     Route::post('/create', [TwUsuariosController::class, 'store'])->middleware('auth:api');
     Route::get('/show/{id}', [TwUsuariosController::class, 'show'])->middleware('auth:api');
     Route::put('/update/{id}', [TwUsuariosController::class, 'update'])->middleware('auth:api');
     Route::delete('/delete/{id}', [TwUsuariosController::class, 'destroy'])->middleware('auth:api');
+});
+
+//CRUD tw_corporativos
+Route::prefix('corporativos')->group(function () {
+    Route::get('/list', [TwCorporativosController::class, 'index'])->middleware('auth:api');
+    Route::post('/create', [TwCorporativosController::class, 'store'])->middleware('auth:api');
+    Route::get('/show/{id}', [TwCorporativosController::class, 'show'])->middleware('auth:api');
+    Route::put('/update/{id}', [TwCorporativosController::class, 'update'])->middleware('auth:api');
+    Route::delete('/delete/{id}', [TwCorporativosController::class, 'destroy'])->middleware('auth:api');
 });
