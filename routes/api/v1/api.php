506fafb8b36486483054b3f12f8547ef969dc23d
Carlos Guzman <alberto.guzman.220793@gmail.com>
2022-04-24 19:37:00 -0500

    se crearon las rutas para enviar los link de restablecimiento con token y la ruta de reset donde pasa sus nuevas contraseñas

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -1,11 +1,18 @@
 <?php
 
-use App\Http\Controllers\api\v1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\v1\AuthController;
+use App\Http\Controllers\api\v1\NewPasswordController;
 
 //Authentication
 Route::prefix('usuarios')->group(function () {
     //Route::post('/login', 'AuthController@login');
     Route::post('/login', [AuthController::class, 'login']);
 });
+
+//Reset Password
+Route::post('forgot-password', [NewPasswordController::class, 'forgotPassword']);
+Route::post('reset-password', [NewPasswordController::class, 'resetPassword']);
+
+
