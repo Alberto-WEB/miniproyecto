94feb143b23f789874cea8088b7964d6987b3bc0
Carlos Guzman <alberto.guzman.220793@gmail.com>
2022-04-25 23:09:07 -0500

    asignacion de datos masiva a modelo

--- app/Models/twDocumento.php
+++ app/Models/twDocumento.php
@@ -9,6 +9,15 @@ class twDocumento extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'S_Nombre',
+        'N_Obligatorio',
+        'S_Descripcion',
+    ];
+
+    const UPDATED_AT = null;
+    const CREATED_AT = null;
+
     //relacion de muchos a muchos
     public function corporativo(){
         return $this->belongsToMany(twCorporativo::class);
